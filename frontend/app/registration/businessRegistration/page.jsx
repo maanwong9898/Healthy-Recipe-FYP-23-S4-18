7369f0efc1fa5d6dc9424f2f1ed212d1a3c9f726
maanwong <43668754+maanwong@users.noreply.github.com>
2023-12-07 18:28:08 +0800

    add business registration page
    - incomplete user registration
    - additional node_modules for datepicker 'npm install date-picker'
    - removed style.css (irrelevant)

--- frontend/app/registration/businessRegistration/page.jsx
+++ frontend/app/registration/businessRegistration/page.jsx
@@ -5,10 +5,204 @@ import Link from "next/link";
 import HomeNavbar from "@/app/components/navigation/homeNavBar";
 
 const businessRegistration = () => {
+  const [fullName, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [workEmail, setWorkEmail] = useState("");
+  const [organization, setOrganization] = useState("");
+  const [uen, setUen] = useState("");
+  const [businessUserType, setBusinessUserType] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPwd, setConfirmPwd] = useState("");
+  const [error, setError] = useState("");
+  const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const handleSignUp = () => {
+    event.preventDefault();
+    // Checks if fields are empty
+    if (
+      !fullName ||
+      !username ||
+      !workEmail ||
+      !organization ||
+      !uen ||
+      !businessUserType ||
+      !password ||
+      !confirmPwd
+    ) {
+      setError("All fields are required.");
+      return;
+      // Checks if the pwd matches
+    } else if (password !== confirmPwd) {
+      setError("Passwords do not match.");
+      return;
+      // Checks if email is in the right format
+    } else if (!emailValidation.test(workEmail)) {
+      setError("Invalid email address.");
+      return;
+    } else {
+      alert("Registration successful!");
+      // Reset fields in the form + error state
+      setFullName("");
+      setUsername("");
+      setWorkEmail("");
+      setOrganization("");
+      setUen("");
+      setBusinessUserType("");
+      setPassword("");
+      setConfirmPwd("");
+      setError("");
+    }
+
+    // To check if data passed is correct
+    console.log("Sign up details:", {
+      fullName,
+      username,
+      workEmail,
+      organization,
+      uen,
+      businessUserType,
+      password,
+      confirmPwd,
+    });
+  };
+
   return (
     <div>
       <HomeNavbar />
-      <h1>this is business registration</h1>
+      <div className="bg-blue-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign Up
+              </h1>
+              <form className="space-y-4 md:space-y-6">
+                <div className="flex space-x-4">
+                  {/* NAME */}
+                  <input
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    placeholder="Full Name"
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  ></input>
+
+                  {/* USERNAME */}
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  ></input>
+                </div>
+
+                {/* WORK EMAIL */}
+                <div>
+                  <input
+                    type="email"
+                    name="workEmail"
+                    id="workEmail"
+                    placeholder="Work Email Address"
+                    value={workEmail}
+                    onChange={(e) => setWorkEmail(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  ></input>
+                </div>
+
+                {/* ORGANIZATION */}
+                <div>
+                  <input
+                    type="text"
+                    name="organization"
+                    id="organization"
+                    placeholder="Organization"
+                    value={organization}
+                    onChange={(e) => setOrganization(e.target.value)}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  ></input>
+                </div>
+
+                <div className="flex space-x-4">
+                  {/* UEN */}
+                  <input
+                    type="text"
+                    name="uen"
+                    id="uen"
+                    placeholder="UEN"
+                    value={uen}
+                    onChange={(e) => setUen(e.target.value)}
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  ></input>
+
+                  {/* USER TYPE */}
+                  <select
+                    id="businessUserType"
+                    name="businessUserType"
+                    value={businessUserType}
+                    onChange={(e) => setBusinessUserType(e.target.value)}
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  >
+                    <option value="" disabled selected>
+                      Register As
+                    </option>
+                    <option value="businessUser">Business User</option>
+                    <option value="dietitian">Dietitian</option>
+                  </select>
+                </div>
+
+                {/* PASSWORDS */}
+                <div className="flex space-x-4">
+                  <input
+                    type="password"
+                    name="pwd"
+                    id="pwd"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  ></input>
+                  <input
+                    type="password"
+                    name="confirmPwd"
+                    id="confirmPwd"
+                    placeholder="Confirm Password"
+                    value={confirmPwd}
+                    onChange={(e) => setConfirmPwd(e.target.value)}
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  ></input>
+                </div>
+                {/* ERROR MSG */}
+                <p className="text-red-500 text-sm">{error}</p>
+
+                {/* SUBMIT BUTTON */}
+                <button
+                  type="submit"
+                  onClick={handleSignUp}
+                  className="bg-blue-500 hover:bg-blue-700 text-white  rounded-md font-bold py-2 px-4 w-full"
+                >
+                  Create an account
+                </button>
+
+                {/* LOGIN LINK */}
+                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Already have an account?{" "}
+                  <a
+                    href="/userLogin"
+                    class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Login here
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/app/registration/userRegistration/page.jsx
+++ frontend/app/registration/userRegistration/page.jsx
@@ -3,12 +3,143 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Link from "next/link";
 import HomeNavbar from "@/app/components/navigation/homeNavBar";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const userRegistration = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [selectedOption, setSelectedOption] = useState("");
+
   return (
     <div>
       <HomeNavbar />
-      <h1>this is user registration</h1>
+      <div className="bg-blue-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign Up
+              </h1>
+              <form className="space-y-4 md:space-y-6">
+                <div className="flex space-x-4">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Full Name"
+                  ></input>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Username"
+                  ></input>
+                </div>
+                <div>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email Address"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  ></input>
+                </div>
+                <div className="flex space-x-4">
+                  {/* <select id="dropdown1" className="border p-2 rounded">
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                  </select>
+
+                  <select id="dropdown2" className="border p-2 rounded">
+                    <option value="optionA">Option A</option>
+                    <option value="optionB">Option B</option>
+                  </select>
+
+                  <select id="dropdown2" className="border p-2 rounded">
+                    <option value="optionA">Option A</option>
+                    <option value="optionB">Option B</option>
+                  </select> */}
+                  <div className="relative max-w-sm">
+                    <label>
+                      Date Of Birth
+                      <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                    </label>
+                  </div>
+                </div>
+                <div>
+                  <label>Dietary Preference</label>
+                  <select
+                    id="dietPreference"
+                    value={selectedOption}
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  >
+                    <option value="">Select Option...</option>
+                    <option value="vegan">Vegan</option>
+                    <option value="Vegetarian">Vegetarian</option>
+                    <option value="Pescatarian">Pescatarian</option>
+                    <option value="Omnivore">Omnivore</option>
+                  </select>
+                </div>
+                <div className="flex space-x-4">
+                  <input
+                    type="text"
+                    name="height"
+                    id="height"
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Height"
+                  ></input>
+                  <input
+                    type="text"
+                    name="weight"
+                    id="weight"
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Weight"
+                  ></input>
+                </div>
+                <div className="flex space-x-4">
+                  <input
+                    type="password"
+                    name="pwd"
+                    id="pwd"
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Password"
+                  ></input>
+                  <input
+                    type="password"
+                    name="confirm-pwd"
+                    id="confirm-pwd"
+                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Confirm Password"
+                  ></input>
+                </div>
+                <button
+                  type="submit"
+                  class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800"
+                >
+                  Create an account
+                </button>
+
+                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Already have an account?{" "}
+                  <a
+                    href="/userLogin"
+                    class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Login here
+                  </a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

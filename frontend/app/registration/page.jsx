7d2e73ff24f310f61c3b4fdac298e16d7eed700c
maanwong <43668754+maanwong@users.noreply.github.com>
2023-12-12 23:54:43 +0800

    Update registration selection page

--- frontend/app/registration/page.jsx
+++ frontend/app/registration/page.jsx
@@ -9,23 +9,26 @@ const RegistrationSelection = () => {
     <div className="bg-blue-900">
       <HomeNavbar />
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="w-full max-w-md bg-blue-300 rounded-xl shadow-md py-16 px-16">
-          <h2 className="text-[28px] font-bold text-black mb-6 text-center">
+        <div className="w-full max-w-lg bg-slate-100 rounded-xl shadow-md py-16 px-16">
+          <h2 className="text-[28px] font-bold text-black mb-6 text-center items-center">
             Sign Up As
           </h2>
-          <form className="flex flex-col items-center space-y-4">
-            <div className="flex space-x-4">
-              <Link href="/registration/userRegistration">
-                <button className="text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                  User
-                </button>
-              </Link>
-              <Link href="/registration/businessRegistration">
-                <button className="text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                  Business User
-                </button>
-              </Link>
-            </div>
+          <form className="flex flex-row items-center justify-center">
+            <Link href="/registration/userRegistration">
+              <button className="text-center text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                User
+              </button>
+            </Link>
+            <Link href="/registration/dietitianRegistration">
+              <button className="text-center text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                Dietitian
+              </button>
+            </Link>
+            <Link href="/registration/businessRegistration">
+              <button className="text-center text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                Business User
+              </button>
+            </Link>
           </form>
         </div>
       </div>

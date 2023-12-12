b0fd35341b8a5c97d9b505e002fb692f008d3b3a
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2023-12-12 15:41:37 +0800

    and another update thats supposed to go in w previous commit...

--- HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/Repository/RecipeReviewRatingRepository.java
+++ HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/Repository/RecipeReviewRatingRepository.java
@@ -1,10 +1,26 @@
 package com.FYP18.HealthyRecipe.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+ 
 import com.FYP18.HealthyRecipe.Entity.RecipeReviewRating;
 import com.FYP18.HealthyRecipe.Entity.RecipeReviewRatingId;
 
 public interface RecipeReviewRatingRepository extends JpaRepository<RecipeReviewRating, RecipeReviewRatingId>  {
     
+    // @Modifying
+    // @Transactional
+    // @Query("SELECT b FROM recipe_review_rating b WHERE b.userid = :id.UserID AND b.recipeid = :id.RecipeID")
+    // List<RecipeReviewRating> findByUserID(String id);
+
+
+    @Modifying
+    @Transactional
+    @Query("SELECT b FROM RecipeReviewRating b WHERE b.recipeReviewRatingId.UserID = :userId")
+    List<RecipeReviewRating> findByUserID(String userId);
+
 }

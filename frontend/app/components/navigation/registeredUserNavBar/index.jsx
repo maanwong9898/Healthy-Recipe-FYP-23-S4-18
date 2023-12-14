4551022f785779f18fcb1ec9c072b313265142b2
maanwong <43668754+maanwong@users.noreply.github.com>
2023-12-14 20:34:56 +0800

    Update registered user nav bar and create side bar for account settings

--- frontend/app/components/navigation/registeredUserNavBar/index.jsx
+++ frontend/app/components/navigation/registeredUserNavBar/index.jsx
@@ -1,20 +1,25 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
-import Link from "next/link";
+import React, { useState, useRef } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const AccountDropdownMenu = () => {
   return (
     <div className="absolute left-0 top-10 w-48 rounded-md shadow-lg bg-white z-10">
       <ul>
         <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-          <Link href="/registeredUser/viewAccount">My Account</Link>
+          <Link href="/registeredUser/userAccount/viewAccount">My Account</Link>
         </li>
         <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-          <Link href="/registeredUser/">Dietary Preference</Link>
+          <Link href="/registeredUser/userAccount">Dietary Preference</Link>
         </li>
         <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-          <Link href="/registeredUser">Track Weight and Measurements</Link>
+          <Link href="/registeredUser/userAccount">
+            Track Weight and Measurements
+          </Link>
+        </li>
+        <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+          <Link href="/registeredUser/userAccount">Check BMI</Link>
         </li>
         <li className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
           <Link href="/">Logout</Link>
@@ -71,71 +76,95 @@ const RegisteredUserNavBar = () => {
   };
 
   return (
-    <nav className="bg-blue-300">
+    <nav className="bg-cyan-600">
       <div className="flex flex-wrap items-center p-3">
         {/* Logo and mobile menu button (small screen) */}
         <div className="flex items-center justify-between w-full md:w-auto md:mr-4">
-          <div className="flex items-center text-blue-950 rounded-md px-3 py-2 text-sm font-extrabold">
+          <div className="flex items-center text-white rounded-md px-3 py-2 text-sm font-extrabold">
             My Healthy Recipe
           </div>
           <button
-            className="text-blue-800 p-2 rounded-md hover:text-white hover:bg-blue-900 md:hidden"
+            className="text-white p-2 rounded-md hover:text-white hover:bg-blue-900 md:hidden"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             {isMenuOpen ? "✖" : "☰"}
           </button>
+          <button
+            className="text-white p-2 rounded-md hover:text-white hover:bg-blue-900 md:hidden"
+            onClick={() => setIsAccountMenuOpen(!isAccountMenuOpen)}
+          >
+            Account Settings
+          </button>
         </div>
 
-        {/* Links for large screens for dietitian nav bar */}
+        {/* Links for large screens */}
         <div
           className={`w-full md:flex md:w-auto ${
             isMenuOpen ? "block" : "hidden"
           }`}
         >
-          <div className="flex flex-col  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+          <div className="flex flex-col  md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium">
             <Link
               href="/"
-              className="text-blue-800 hover:bg-blue-900 hover:text-white rounded-md px-3 py-2 text-sm font-bold"
+              className="text-white hover:bg-sky-200 hover:text-black rounded-md px-3 py-2 text-sm font-bold"
             >
               Home
             </Link>
-            <Link
-              href="/aboutUs"
-              className="text-blue-800 hover:bg-blue-900 hover:text-white rounded-md px-3 py-2 text-sm font-bold"
+            <div
+              className="relative flex items-center"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             >
-              About Us
-            </Link>
+              <span className="text-white hover:bg-sky-200 hover:text-black rounded-md px-3 py-2 text-sm font-bold cursor-pointer">
+                About Us
+              </span>
+            </div>
             <Link
-              href="/recipes"
-              className="text-blue-800 hover:bg-blue-900 hover:text-white rounded-md px-3 py-2 text-sm font-bold"
+              href="/"
+              className="text-white hover:bg-sky-200 hover:text-black rounded-md px-3 py-2 text-sm font-bold"
             >
               Recipes
             </Link>
             <Link
-              href="/mealPlan"
-              className="text-blue-800 hover:bg-blue-900 hover:text-white rounded-md px-3 py-2 text-sm font-bold"
+              href="/"
+              className="text-white hover:bg-sky-200 hover:text-black rounded-md px-3 py-2 text-sm font-bold"
             >
               Meal Plans
             </Link>
             <Link
               href="/"
-              className="text-blue-800 hover:bg-blue-900 hover:text-white rounded-md px-3 py-2 text-sm font-bold"
+              className="text-white hover:bg-sky-200 hover:text-black rounded-md px-3 py-2 text-sm font-bold"
             >
               Educational Content
             </Link>
             <Link
               href="/"
-              className="text-blue-800 hover:bg-blue-900 hover:text-white rounded-md px-3 py-2 text-sm font-bold"
+              className="text-white hover:bg-sky-200 hover:text-black rounded-md px-3 py-2 text-sm font-bold"
             >
-              Business Blog Post
+              Business Blog Posts
             </Link>
+            <button
+              onClick={confirmAndLogout}
+              className="block md:hidden py-2 pr-4 pl-3 text-blue-800 rounded md:bg-transparent md:ml-auto md:p-0"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
 
+        {/* Links for large screens for Acccounts*/}
+        <div
+          className={`w-full md:flex md:w-auto md:ml-auto mr-20 ${
+            isAccountMenuOpen ? "block" : "hidden"
+          }`}
+        >
+          <div className="flex flex-col md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <div
               className="relative flex items-center"
               onMouseEnter={handleMouseEnterAccount}
               onMouseLeave={handleMouseLeaveAccount}
             >
-              <span className="md:ml-auto text-blue-800 hover:bg-blue-900 hover:text-white rounded-md px-3 py-2 text-sm font-bold cursor-pointer">
+              <span className="hidden md:block text-white hover:bg-sky-200 hover:text-black rounded-md px-3 py-2 text-sm font-bold cursor-pointer">
                 Account Settings
               </span>
               {isAccountDropdownVisible && <AccountDropdownMenu />}
---
+++ frontend/app/components/navigation/registeredUserSideBar/index.jsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const SideBarLayout = () => {
+  //Expand my account
+  //   const [isExpanded, setIsExpanded] = useState(false);
+  //   const ExpandMyAccount = () => {
+  //     setIsExpanded(!isExpanded);
+  //   };
+
+  return (
+    <div className="w-1/4 h-screen bg-gray-200 p-4">
+      <h2 className="text-lg font-semibold mb-10">My Dashboard</h2>
+      <ul>
+        <li className="mb-4 hover:underline">
+          <Link href="/registeredUser/userAccount/viewAccount">My Account</Link>
+        </li>
+
+        <li className="mb-4 hover:underline m-4">
+          <Link href="/registeredUser/userAccount/changePwd">
+            Change Password
+          </Link>
+        </li>
+
+        <li className="mb-4 hover:underline">
+          <Link href="/">Dietary Preference</Link>
+        </li>
+        <li className="mb-4 hover:underline">
+          <Link href="/">Track Weight</Link>
+        </li>
+        <li className="mb-4 hover:underline">
+          <Link href="/">BMI Calculator</Link>
+        </li>
+        <li className="mb-4 hover:underline">
+          <Link href="/">Logout</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default SideBarLayout;

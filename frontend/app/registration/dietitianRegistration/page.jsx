2966fc296fbc7b9dcd113caeae2b6aae09837e49
maanwong <43668754+maanwong@users.noreply.github.com>
2023-12-18 14:58:50 +0800

    update dietitian registration page

--- frontend/app/registration/dietitianRegistration/page.jsx
+++ frontend/app/registration/dietitianRegistration/page.jsx
@@ -82,7 +82,7 @@ const DietitianRegistration = () => {
     <div>
       <HomeNavbar />
       <div className="bg-cyan-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen">
           <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold text-gray-900 md:text-2xl">

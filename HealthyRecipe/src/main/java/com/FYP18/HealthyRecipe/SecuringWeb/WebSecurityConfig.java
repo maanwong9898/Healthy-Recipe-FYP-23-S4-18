157d5a477cac45838d0b6614d58d201d162dbb33
maanwong <43668754+maanwong@users.noreply.github.com>
2024-01-08 18:34:13 +0800

    Update registered_user role and localhost link

--- HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/WebSecurityConfig.java
+++ HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/WebSecurityConfig.java
@@ -30,28 +30,30 @@ import com.FYP18.HealthyRecipe.Service.CustomUserDetailService;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    // passwordEncoder -> securityFilterChain -> configure -> filterChain -> authenticationManager
+    // passwordEncoder -> securityFilterChain -> configure -> filterChain ->
+    // authenticationManager
     // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+    // Exception
     // {
-    //     System.out.println("securityFilterChain");
-    //     http
-    //     .authorizeHttpRequests((authorize) -> authorize
-        
-    //         // .requestMatchers("/login").permitAll()
-    //         .anyRequest().authenticated()
-    //     );
+    // System.out.println("securityFilterChain");
+    // http
+    // .authorizeHttpRequests((authorize) -> authorize
 
-    //     return http.build();
-    // }  
+    // // .requestMatchers("/login").permitAll()
+    // .anyRequest().authenticated()
+    // );
+
+    // return http.build();
+    // }
     // private final CustomUserDetailService userDetailsService;
     // private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
     // public WebSecurityConfig(CustomUserDetailService customUserDetailsService)
-    // //, JwtAuthorizationFilter jwtAuthorizationFilter) 
+    // //, JwtAuthorizationFilter jwtAuthorizationFilter)
     // {
-    //     this.userDetailsService = customUserDetailsService;
-    //     // this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+    // this.userDetailsService = customUserDetailsService;
+    // // this.jwtAuthorizationFilter = jwtAuthorizationFilter;
     // }
 
     // @Autowired
@@ -60,87 +62,88 @@ public class WebSecurityConfig {
     // @Bean
     // public CustomUserDetailService userDetailService()
     // {
-    //     return new CustomUserDetailService(userRepository);
+    // return new CustomUserDetailService(userRepository);
     // }
 
-	@Bean
+    @Bean
     @SuppressWarnings("deprecation")
-	public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
 
         System.out.println("passwordEncoder");
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-		// return new  BCryptPasswordEncoder(); // PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
-    @Bean
-	public AuthenticationManager authenticationManager(  UserDetailsService userDetailsService,
-                    PasswordEncoder passwordEncoder) {
+        // return new BCryptPasswordEncoder(); //
+        // PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
 
         System.out.println("authenticationManager");
 
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(userDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder); 
-		return new ProviderManager(authenticationProvider);
-	}
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authenticationProvider);
+    }
 
     // @Bean
     // public SecurityFilterChain configure (HttpSecurity http) throws Exception
     // {
-    //     System.out.println("configure");
-    //     http
-    //         .csrf(csrf-> csrf.disable())
-    //         .sessionManagement(x-> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
-    //         .authorizeHttpRequests((authz)-> 
-    //             authz
-    //                 .anyRequest()
-    //                 // .requestMatchers("/public")
-    //                 .permitAll()
-    //                 // .anyRequest()
-    //                 // .authenticated()
-    //                 );
+    // System.out.println("configure");
+    // http
+    // .csrf(csrf-> csrf.disable())
+    // .sessionManagement(x->
+    // x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    // .authorizeHttpRequests((authz)->
+    // authz
+    // .anyRequest()
+    // // .requestMatchers("/public")
+    // .permitAll()
+    // // .anyRequest()
+    // // .authenticated()
+    // );
 
-            
-    //     return http.build();
+    // return http.build();
     // }
     // @Bean
     // public UserDetailsService users()
     // {
-    //     UserDetails admin = User.builder()
-    //                         .username("admin")
-    //                         .password("pw")
-    //                         .roles("ADMIN")
-    //                         .build(); 
-    //     UserDetails user = User.builder()
-    //                         .username("user")
-    //                         .password("pw")
-    //                         .roles("USER")
-    //                         .build(); 
-    //     return new InMemoryUserDetailsManager(admin, user);
+    // UserDetails admin = User.builder()
+    // .username("admin")
+    // .password("pw")
+    // .roles("ADMIN")
+    // .build();
+    // UserDetails user = User.builder()
+    // .username("user")
+    // .password("pw")
+    // .roles("USER")
+    // .build();
+    // return new InMemoryUserDetailsManager(admin, user);
     // }
 
-    //    @Bean
+    // @Bean
     // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    //     System.out.println("filterChain");
-        
-    //     http.authorizeHttpRequests(req->
-    //     req.anyRequest().authenticated());
+    // System.out.println("filterChain");
 
-    //     // http 
-    //     // .csrf()
-    //     //     .formLogin()
-    //     //     .and()
-    //     //     .headers().disable()
-    //     //     .csrf().disable()
-    //     //     .antMatcher("/**")
-    //         // .authorizeRequests()
-    //         //     .antMatchers("/public").permitAll()
-    //         //     .antMatchers("/h2-console/**").permitAll()
-    //         //     //...
-    //         //     .anyRequest()
-    //         //     .authenticated();
-    //     return http.build();
+    // http.authorizeHttpRequests(req->
+    // req.anyRequest().authenticated());
+
+    // // http
+    // // .csrf()
+    // // .formLogin()
+    // // .and()
+    // // .headers().disable()
+    // // .csrf().disable()
+    // // .antMatcher("/**")
+    // // .authorizeRequests()
+    // // .antMatchers("/public").permitAll()
+    // // .antMatchers("/h2-console/**").permitAll()
+    // // //...
+    // // .anyRequest()
+    // // .authenticated();
+    // return http.build();
     // }
     @Autowired
     public JwtAuthorizationFilter jwtAuthorizationFilter;
@@ -149,27 +152,30 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         System.out.println("FilterChain");
         http
-            .csrf(csrf-> csrf.disable())
-            // Disabling CSRF protection means that Spring Security won't generate and validate these tokens. 
-            // While this solves the issue for now, it's important to understand that disabling CSRF protection 
-            // can expose your application to CSRF attacks. It's generally recommended to keep CSRF protection 
-            // enabled and properly configure your frontend code to include and send the CSRF token with each request.
+                .csrf(csrf -> csrf.disable())
+                // Disabling CSRF protection means that Spring Security won't generate and
+                // validate these tokens.
+                // While this solves the issue for now, it's important to understand that
+                // disabling CSRF protection
+                // can expose your application to CSRF attacks. It's generally recommended to
+                // keep CSRF protection
+                // enabled and properly configure your frontend code to include and send the
+                // CSRF token with each request.
 
-            .authorizeHttpRequests((authz) -> authz
-                // .anyRequest().permitAll()
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/test/admin").hasRole("ADMIN")
-                .requestMatchers("/test/business").hasRole("BUSINESS_USER")
-                .requestMatchers("/test/nut").hasRole("NUTRITIONIST")
-                .requestMatchers("/test/user").hasRole("USER") 
-                .requestMatchers("/test/userAndAdmin").hasAnyRole("ADMIN", "USER")
-                .requestMatchers("/register/**").permitAll()
-                .anyRequest().permitAll()
-            )
-           
-            .sessionManagement(x-> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
-        
+                .authorizeHttpRequests((authz) -> authz
+                        // .anyRequest().permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/test/admin").hasRole("ADMIN")
+                        .requestMatchers("/test/business").hasRole("BUSINESS_USER")
+                        .requestMatchers("/test/nut").hasRole("NUTRITIONIST")
+                        .requestMatchers("/test/user").hasRole("REGISTERED_USER")
+                        .requestMatchers("/test/userAndAdmin").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/register/**").permitAll()
+                        .anyRequest().permitAll())
+
+                .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
-    } 
+    }
 }

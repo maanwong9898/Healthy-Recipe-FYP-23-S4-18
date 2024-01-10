cc871d2ae136e1066e9e41dcdf25a12e4d146c05
maanwong <43668754+maanwong@users.noreply.github.com>
2024-01-10 15:19:24 +0800

    update business, nutritionist registration page and selection of user page (UI)

--- frontend/app/registration/businessRegistration/page.jsx
+++ frontend/app/registration/businessRegistration/page.jsx
@@ -1,26 +1,25 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import HomeNavbar from "@/app/components/navigation/homeNavBar";
 
-const BusinessRegistration = () => {
+const businessRegistration = () => {
   const [fullName, setFullName] = useState("");
   const [username, setUsername] = useState("");
-  const [workEmail, setWorkEmail] = useState("");
-  const [organization, setOrganization] = useState("");
-  const [uen, setUen] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPwd, setConfirmPwd] = useState("");
   const [contactNumber, setContactNumber] = useState("");
-  const [address, setAddress] = useState("");
+  const [workEmail, setWorkEmail] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [companyAddress, setCompanyAddress] = useState("");
+  const [uen, setUen] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  const handleSignUp = (event) => {
-    event.preventDefault();
-    // Checks if fields are empty
+  const handleSignUp = () => {
     if (
       fullName === "" ||
       username === "" ||
@@ -28,18 +27,20 @@ const BusinessRegistration = () => {
       confirmPwd === "" ||
       contactNumber === "" ||
       workEmail === "" ||
-      organization === "" ||
-      address === "" ||
+      companyName === "" ||
+      companyAddress === "" ||
       uen === ""
     ) {
       setError("All fields are required.");
       return;
-      // Checks if the pwd matches
-    } else if (password !== confirmPwd) {
+    }
+    // Checks if the pwd matches
+    else if (password !== confirmPwd) {
       setError("Passwords do not match.");
       return;
-      // Checks if email is in the right format
-    } else if (!emailValidation.test(workEmail)) {
+    }
+    // Checks if email is in the right format
+    else if (!emailValidation.test(workEmail)) {
       setError("Invalid email address.");
       return;
     } else {
@@ -57,184 +58,223 @@ const BusinessRegistration = () => {
       setConfirmPwd("");
       setContactNumber("");
       setWorkEmail("");
-      setOrganization("");
-      setAddress("");
+      setCompanyName("");
+      setCompanyAddress("");
       setUen("");
       setError("");
     }
-
-    // To check if data passed is correct
-    console.log("Business Sign up details:", {
-      fullName,
-      username,
-      password,
-      confirmPwd,
-      contactNumber,
-      workEmail,
-      organization,
-      address,
-      uen,
-    });
   };
 
   return (
     <div>
       <HomeNavbar />
-      <div className="bg-cyan-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen">
-          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold text-gray-900 md:text-2xl">
-                Business User Sign Up
-              </h1>
-              <form className="space-y-2">
-                <h2>Personal Details</h2>
-                <div className="flex space-x-4">
-                  {/* NAME */}
-                  <input
-                    type="text"
-                    name="fullName"
-                    id="fullName"
-                    placeholder="Full Name"
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
-                    className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  ></input>
-
-                  {/* USERNAME */}
-                  <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                  ></input>
-                </div>
-
-                {/* PASSWORDS */}
-                <div className="flex space-x-4">
-                  <input
-                    type="password"
-                    name="pwd"
-                    id="pwd"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm block w-full p-2.5"
-                  ></input>
-                  <input
-                    type="password"
-                    name="confirmPwd"
-                    id="confirmPwd"
-                    placeholder="Confirm Password"
-                    value={confirmPwd}
-                    onChange={(e) => setConfirmPwd(e.target.value)}
-                    className="border px-4 py-2 rounded-lg bg-gray-50 border-gray-300 text-gray-900 sm:text-sm block w-full p-2.5"
-                  ></input>
-                </div>
-
-                {/* CONTACT NUMBER */}
-                <div className="flex flex-row">
-                  <input
-                    type="text"
-                    name="contactNumber"
-                    id="contactNumber"
-                    placeholder="Contact Number"
-                    value={contactNumber}
-                    onChange={(e) => setContactNumber(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  ></input>
-                </div>
-
-                <h2>Organization Details</h2>
-                {/* WORK EMAIL */}
-                <div>
-                  <input
-                    type="email"
-                    name="workEmail"
-                    id="workEmail"
-                    placeholder="Work Email Address"
-                    value={workEmail}
-                    onChange={(e) => setWorkEmail(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                  ></input>
-                </div>
-
-                {/* ORGANIZATION */}
-                <div>
-                  <input
-                    type="text"
-                    name="organization"
-                    id="organization"
-                    placeholder="Company Name"
-                    value={organization}
-                    onChange={(e) => setOrganization(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  ></input>
-                </div>
-
-                {/* ADDRESS */}
-                <div className="flex flex-row">
-                  <input
-                    type="text"
-                    name="address"
-                    id="address"
-                    placeholder="Company Address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  ></input>
-                </div>
-
-                {/* UEN */}
-                <div className="flex flex-row">
-                  <input
-                    type="text"
-                    name="uen"
-                    id="uen"
-                    placeholder="UEN"
-                    value={uen}
-                    onChange={(e) => setUen(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  ></input>
-                </div>
-
-                {/* ERROR MSG */}
-                <p className="text-red-500 text-sm">{error}</p>
-                <p className="text-green-500 text-sm">{success}</p>
-
-                {/* SUBMIT BTN */}
-                <button
-                  type="submit"
-                  onClick={handleSignUp}
-                  className=" text-white bg-cyan-500 hover:bg-sky-700 rounded-md font-bold py-2 px-4 w-full"
-                >
-                  Create an account
-                </button>
-
+      <div className="bg-orange-50 min-h-screen py-16">
+        <div className="container mx-auto">
+          <div className="flex flex-col md:flex-row rounded-xl mx-auto bg-slate-200 shadow-lg overflow-hidden">
+            {/* IMG + REGISTER AS ANOTHER USER */}
+            <div className="w-full md:w-1/2 p-4 md:p-10 bg-white">
+              <div className="text-center">
+                <h1 className="font-semibold text-3xl">
+                  Sign Up as a Business User
+                </h1>
+                <p className="text-sm font-light text-black">
+                  Sign up as a different user{" "}
+                  <Link
+                    href="/registration"
+                    className="font-medium text-blue-600 hover:underline"
+                  >
+                    Sign up here
+                  </Link>
+                </p>
                 {/* LOGIN LINK */}
                 <p className="text-sm font-light text-black">
                   Already have an account?{" "}
                   <Link
                     href="/userLogin"
-                    className="font-medium text-cyan-600 hover:underline"
+                    className="font-medium text-blue-600 hover:underline"
                   >
                     Login here
                   </Link>
                 </p>
-                {/* SIGN UP LINK */}
-                <p className="text-sm font-light text-black">
-                  Sign up as a different user{" "}
-                  <Link
-                    href="/registration"
-                    className="font-medium text-cyan-600 hover:underline"
-                  >
-                    Sign up here
-                  </Link>
-                </p>
-              </form>
+              </div>
+
+              <div
+                className="h-full w-full bg-cover bg-center flex items-center justify-center flex-col text-center p-8"
+                style={{ backgroundImage: `url('/business_registration.jpg')` }}
+              ></div>
+            </div>
+            {/* REGISTRATION FORM */}
+            <div className="w-full md:w-1/2 mx-5 flex items-center justify-center">
+              <div className="w-full p-4 md:p-10">
+                <h1 className="text-3xl md:text-4xl font-semibold mb-4">
+                  Create an Account
+                </h1>
+                <form action={handleSignUp}>
+                  <div className="grid grid-cols-2 gap-3 mt-2">
+                    <label htmlFor="fullName" className="flex items-center">
+                      Full Name
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <label htmlFor="username" className="flex items-center">
+                      Username
+                      <span className="text-red-500">*</span>
+                    </label>
+                  </div>
+                  <div className="grid grid-cols-2 gap-3">
+                    <input
+                      type="text"
+                      id="fullName"
+                      name="fullName"
+                      placeholder="Your Name"
+                      className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+
+                    <input
+                      type="text"
+                      id="userName"
+                      name="userName"
+                      placeholder="Your Username"
+                      className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+                  </div>
+
+                  <div className="grid grid-cols-2 gap-3 mt-2">
+                    <label htmlFor="password" className="flex items-center">
+                      Password
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <label
+                      htmlFor="repeatPassword"
+                      className="flex items-center"
+                    >
+                      Repeat Password
+                      <span className="text-red-500">*</span>
+                    </label>
+                  </div>
+                  <div className="grid grid-cols-2 gap-3">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="Password"
+                      className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+
+                    <input
+                      type="password"
+                      id="repeatPassword"
+                      name="repeatPassword"
+                      placeholder="Repeat Password"
+                      className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+                  </div>
+
+                  <div className="mt-3">
+                    <label
+                      htmlFor="contactNumber"
+                      className="flex items-center"
+                    >
+                      Contact Number
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      id="contactNumber"
+                      name="contactNumber"
+                      placeholder="+65 1234 5678"
+                      className=" bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+                  </div>
+
+                  <div className="grid grid-cols-2 gap-3 mt-2">
+                    <label htmlFor="workEmail" className="flex items-center">
+                      Work Email
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <label htmlFor="uen" className="flex items-center">
+                      UEN
+                      <span className="text-red-500">*</span>
+                    </label>
+                  </div>
+
+                  <div className="grid grid-cols-2 gap-3">
+                    <input
+                      type="email"
+                      id="workEmail"
+                      name="workEmail"
+                      placeholder="Work Email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+
+                    <input
+                      type="text"
+                      id="uen"
+                      name="uen"
+                      placeholder="UEN"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <label htmlFor="companyName" className="flex items-center">
+                      Company Name
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      id="companyName"
+                      name="companyName"
+                      placeholder="Company Name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <label
+                      htmlFor="companyAddress"
+                      className="flex items-center"
+                    >
+                      Company Address
+                      <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      id="companyAddress"
+                      name="companyAddress"
+                      placeholder="Company Address"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-2.5"
+                      // value=
+                      // onChange={}
+                    />
+                  </div>
+                  {/* ERROR MSG */}
+                  <p className="text-red-500 text-sm">{error}</p>
+
+                  <div className="flex flex-row justify-center">
+                    <button
+                      type="submit"
+                      onChange={handleSignUp}
+                      className="text-white bg-blue-600 hover:bg-blue-700 rounded-md font-bold w-full py-2 px-4 mt-4"
+                    >
+                      Create an Account
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
@@ -243,4 +283,4 @@ const BusinessRegistration = () => {
   );
 };
 
-export default BusinessRegistration;
+export default businessRegistration;

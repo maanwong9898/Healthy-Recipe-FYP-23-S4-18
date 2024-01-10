e55a6775e6e6eecc225569380908ff66597a65e1
Chong Yao Hui <YAO.CHONG891@uowmail.edu.au>
2024-01-10 23:08:35 +0800

    Complete sysAdmin category (connect to controller)

--- frontend/app/sysAdmin/allCategory/page.jsx
+++ frontend/app/sysAdmin/allCategory/page.jsx
@@ -1,80 +1,164 @@
 "use client";
-import React, { useState } from "react";
-
-// router path: /sysAdmin/allCategory
-// This page is for system admin to create subcategories
-
-// Mock data for categories and subcategories
-const mockData = {
-  dietaryPreferences: [
-    { id: "vegan", name: "Vegan" },
-    { id: "vegetarian", name: "Vegetarian" },
-    { id: "pescatarian", name: "Pescatarian" },
-  ],
-  healthGoals: [
-    { id: "weightLoss", name: "Weight Loss" },
-    { id: "maintainHealth", name: "Maintain Health" },
-    { id: "weightGain", name: "Weight Gain" },
-  ],
-  allergiesAndRestrictions: [
-    { id: "milk", name: "Milk" },
-    { id: "egg", name: "Egg" },
-    { id: "soy", name: "Soy" },
-    { id: "shellfish", name: "Shellfish" },
-    { id: "fish", name: "Fish" },
-    { id: "peanut", name: "Peanut" },
-    { id: "treeNut", name: "Tree Nuts" },
-    { id: "Gluten", name: "Gluten" },
-  ],
-  businessBlogPosts: [
-    { id: "cookbook", name: "Cookbook" },
-    { id: "kitchenware", name: "Kitchenware" },
-    { id: "miscellaneous", name: "Miscellaneous" },
-  ],
-  educationalContent: [
-    { id: "healthyEating", name: "Healthy Eating" },
-    { id: "healthyLifestyle", name: "Healthy Lifestyle" },
-  ],
-};
+import axiosInterceptorInstance from "../../axiosInterceptorInstance.js";
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 const CategoriesPage = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [subcategories, setSubcategories] = useState([]);
   const [newSubcategoryName, setNewSubcategoryName] = useState("");
+  const [editingSubcategoryId, setEditingSubcategoryId] = useState(null);
+  const [editedSubcategoryName, setEditedSubcategoryName] = useState("");
 
-  const handleCategoryChange = (event) => {
-    const category = event.target.value;
-    setSelectedCategory(category);
-    setSubcategories(mockData[category] || []);
-    setNewSubcategoryName(""); // Reset new subcategory name
+  // Fetch subcategories whenever the selected category changes
+  useEffect(() => {
+    if (selectedCategory) {
+      fetchSubcategories(selectedCategory);
+    }
+  }, [selectedCategory]);
+
+  // Mapping of categories to their respective API endpoints
+  const categoryToEndpointMap = {
+    DietaryPreferences: "DietaryPreferences",
+    HealthGoals: "HealthGoals",
+    Allergies: "Allergies",
+    BusinessBlogPosts: "BlogPostCategories",
+    EducationalContent: "EducationalContentCategories",
   };
 
-  const handleCreateSubcategory = () => {
-    if (newSubcategoryName.trim()) {
-      // Assuming IDs are generated uniquely, here we just lowercase the name
-      const newSubcategory = {
-        id: newSubcategoryName.toLowerCase().replace(/\s+/g, ""),
-        name: newSubcategoryName,
-      };
-      setSubcategories([...subcategories, newSubcategory]);
-      setNewSubcategoryName(""); // Reset input field after adding
+  // Function to fetch subcategories based on the selected category
+  const fetchSubcategories = async (category) => {
+    try {
+      const endpointName = categoryToEndpointMap[category];
+      const endpoint = `/category/getAll${endpointName}`;
+      const response = await axiosInterceptorInstance.get(endpoint);
+      setSubcategories(response.data || []);
+    } catch (error) {
+      console.error("Error fetching subcategories:", error);
     }
   };
 
+  // Function to handle category change in dropdown
+  const handleCategoryChange = (event) => {
+    const category = event.target.value;
+    setSelectedCategory(category);
+    fetchSubcategories(category);
+    setNewSubcategoryName(""); // Reset new subcategory name
+  };
+
+  // Function to handle creation of a new subcategory
+  const handleCreateSubcategory = async () => {
+    if (newSubcategoryName.trim()) {
+      try {
+        await postSubcategory(selectedCategory, newSubcategoryName);
+        await fetchSubcategories(selectedCategory); // Refetch subcategories
+        setNewSubcategoryName(""); // Reset input field after adding
+      } catch (error) {
+        console.error("Error creating subcategory:", error);
+      }
+    }
+  };
+
+  // Function to post a new subcategory to the server
+  const postSubcategory = async (categoryType, subcategoryName) => {
+    const endpointMap = {
+      DietaryPreferences: "category/dietaryPreference",
+      HealthGoals: "category/healthGoal",
+      Allergies: "category/allergy",
+      BusinessBlogPosts: "category/blogPost",
+      EducationalContent: "category/educational",
+    };
+
+    const endpoint = endpointMap[categoryType];
+    const data = { subcategoryName };
+
+    if (endpoint) {
+      console.log("Create category Posting to endpoint:", endpoint);
+      const response = await axiosInterceptorInstance.post(endpoint, data);
+      return response.data;
+    } else {
+      throw new Error("Invalid category type");
+    }
+  };
+
+  // Function to initiate editing of a subcategory
+  const handleEditSubcategory = (id, name) => {
+    setEditingSubcategoryId(id);
+    setEditedSubcategoryName(name);
+  };
+
+  // Function to save edits made to a subcategory
+  const handleSaveSubcategory = async (id) => {
+    const payload = {
+      id: id,
+      subcategoryName: editedSubcategoryName,
+    };
+
+    const endpointMap = {
+      DietaryPreferences: "category/dietaryPreferences/edit",
+      HealthGoals: "category/healthGoal/edit",
+      Allergies: "category/allergy/edit",
+      BusinessBlogPosts: "category/blogPostCategory/edit",
+      EducationalContent: "category/educationalContentCategory/edit",
+    };
+
+    try {
+      const endpoint = endpointMap[selectedCategory];
+      if (!endpoint) {
+        throw new Error("Invalid category type");
+      }
+      const response = await axiosInterceptorInstance.put(endpoint, payload);
+      // Update the subcategories state here if necessary
+      await fetchSubcategories(selectedCategory); // Refetch subcategories
+      setEditingSubcategoryId(null); // Reset editing state
+    } catch (error) {
+      console.error("Error updating subcategory:", error);
+    }
+  };
+
+  // Function to handle deletion of a subcategory
+  const handleDeleteSubcategory = async (id) => {
+    // Endpoint mapping based on category
+    const deleteEndpointMap = {
+      DietaryPreferences: `/category/deleteDietaryPreference/${id}`,
+      HealthGoals: `/category/deleteHealthGoal/${id}`,
+      Allergies: `/category/deleteAllergy/${id}`,
+      BusinessBlogPosts: `/category/deleteBlogPostCategory/${id}`,
+      EducationalContent: `/category/deleteEducationalContent/${id}`,
+    };
+
+    try {
+      const endpoint = deleteEndpointMap[selectedCategory];
+      if (!endpoint) {
+        throw new Error("Invalid category type");
+      }
+      await axiosInterceptorInstance.delete(endpoint);
+      await fetchSubcategories(selectedCategory); // Refetch subcategories to reflect deletion
+    } catch (error) {
+      console.error("Error deleting subcategory:", error);
+    }
+  };
+
+  // Define categories directly
+  const categories = [
+    "DietaryPreferences",
+    "HealthGoals",
+    "Allergies",
+    "BusinessBlogPosts",
+    "EducationalContent",
+  ];
+
   // Utility function to format the display names for categories
   const formatDisplayName = (name) => {
-    return (
-      name
-        // Insert a space before all caps
-        .replace(/([A-Z])/g, " $1")
-        // Uppercase the first character
-        .replace(/^./, (str) => str.toUpperCase())
-        .trim()
-    );
+    return name
+      .replace(/([A-Z])/g, " $1") // Insert a space before all caps
+      .replace(/^./, (str) => str.toUpperCase()) // Uppercase the first character
+      .trim();
   };
 
   return (
-    <div className=" bg-cyan-800 w-full min-h-screen p-4">
+    <div className="bg-cyan-800 w-full min-h-screen p-4">
       <h1 className="text-3xl text-white font-bold mb-4">Select a Category</h1>
       <select
         value={selectedCategory}
@@ -82,9 +166,9 @@ const CategoriesPage = () => {
         className="p-2 rounded border-2 mb-4"
       >
         <option value="">-- Select a Category --</option>
-        {Object.keys(mockData).map((categoryKey) => (
+        {categories.map((categoryKey) => (
           <option key={categoryKey} value={categoryKey}>
-            {formatDisplayName(categoryKey)} {/* Use the utility function */}
+            {formatDisplayName(categoryKey)}
           </option>
         ))}
       </select>
@@ -101,28 +185,75 @@ const CategoriesPage = () => {
             />
             <button
               onClick={handleCreateSubcategory}
-              className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700  font-bold py-2 px-4 rounded"
+              className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
             >
               Create
             </button>
           </div>
 
           <h2 className="text-2xl font-bold capitalize mb-5 text-white">
-            {selectedCategory.replace(/([A-Z])/g, " $1").trim()} Category
+            {formatDisplayName(selectedCategory)} Category
           </h2>
           <table className="sm:w-3/4 md:w-2/6 w-full border-black border-2">
             <thead className="bg-cyan-600 font-semibold text-cyan-950 border-black border-2">
               <tr>
                 <th className="px-4 py-2 text-xl text-left">Subcategories</th>
+                <th className="px-4 py-2 text-xl text-left"></th>
+                <th className="px-4 py-2 text-xl text-left"></th>
               </tr>
             </thead>
             <tbody>
               {subcategories.map((subcategory) => (
                 <tr
                   key={subcategory.id}
-                  className="border-t bg-white border-b border-blue dark:border-blue-600"
+                  className={`border-t bg-white border-b border-blue dark:border-blue-600 ${
+                    editingSubcategoryId === subcategory.id ? "bg-gray-300" : ""
+                  }`}
                 >
-                  <td className="px-4 py-2 text-base">{subcategory.name}</td>
+                  <td className="px-4 py-9 text-base">
+                    {editingSubcategoryId === subcategory.id ? (
+                      <input
+                        type="text"
+                        value={editedSubcategoryName}
+                        onChange={(e) =>
+                          setEditedSubcategoryName(e.target.value)
+                        }
+                        className="p-2 rounded border-2"
+                      />
+                    ) : (
+                      subcategory.subcategoryName
+                    )}
+                  </td>
+                  <td>
+                    {editingSubcategoryId === subcategory.id ? (
+                      <button
+                        className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
+                        onClick={() => handleSaveSubcategory(subcategory.id)}
+                      >
+                        Save
+                      </button>
+                    ) : (
+                      <button
+                        className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
+                        onClick={() =>
+                          handleEditSubcategory(
+                            subcategory.id,
+                            subcategory.subcategoryName
+                          )
+                        }
+                      >
+                        Edit
+                      </button>
+                    )}
+                  </td>
+                  <td>
+                    <button
+                      className="text-white bg-red-600 border-2 border-black font-bold py-2 px-4 rounded"
+                      onClick={() => handleDeleteSubcategory(subcategory.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -134,135 +265,3 @@ const CategoriesPage = () => {
 };
 
 export default CategoriesPage;
-
-// "use client";
-
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import Link from "next/link";
-
-// // router path: /sysAdmin/createDietaryPreferences
-
-// const mockDietaryPreferences = [
-//   { id: "d18422a6-0ed3-4d08-803f-28e679fcd9aa", name: "Vegetarian" },
-//   { id: "a073d593-1f0c-48fd-87d7-d68d04726cbf", name: "Vegan" },
-//   { id: "a09585d9-8254-44e6-a266-bfeff9549a51", name: "Pescatarian" },
-//   { id: "ee32f7bb-3ca8-493e-aae4-e37944d5cf54", name: "Omnivore/Normal Diet" },
-// ];
-
-// const CreateDietaryPreferences = () => {
-//   const [dietaryPreferences, setDietaryPreferences] = useState(
-//     mockDietaryPreferences
-//   );
-
-//   const [newPreference, setNewPreference] = useState("");
-
-//   //the url will be changed to the backend url
-//   const viewDietaryPreference = async () => {
-//     try {
-//       const response = await fetch(
-//         "http://localhost:8080/api/dietarypreference/readAllDietaryPreferences"
-//       );
-//       if (response.ok) {
-//         const data = await response.json();
-//         // write the data into console for debugging
-//         console.log("data from backend");
-//         console.log(data);
-//         setDietaryPreferences(data);
-//       } else {
-//         console.error("Failed to fetch dietary preference list");
-//       }
-//     } catch (error) {
-//       console.error("Error fetching data", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     viewDietaryPreference();
-//   }, []);
-
-//   const handleCreateDietaryPreferences = async () => {
-//     // Check if the newPreference is not empty
-//     if (newPreference.trim()) {
-//       try {
-//         // Construct the object to send to the backend
-//         console.log("inside handleCreateDietaryPreferences");
-//         console.log(newPreference);
-//         const preferenceToCreate = { name: newPreference };
-
-//         const response = await fetch(
-//           "http://localhost:8080/api/dietarypreference/createDietaryPreference",
-//           {
-//             method: "POST",
-//             headers: {
-//               "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify(preferenceToCreate),
-//           }
-//         );
-
-//         if (response.ok) {
-//           // Fetch the updated list from the backend
-//           await viewDietaryPreference();
-//           setNewPreference(""); // Clear the input field after successful creation
-//         } else {
-//           console.error("Failed to create dietary preference");
-//         }
-//       } catch (error) {
-//         console.error("Error creating dietary preference", error);
-//       }
-//     }
-//   };
-
-//   return (
-//     <div className="bg-cyan-800  min-h-screen flex flex-col justify-center items-center py-5">
-//       <h1 className="text-2xl text-white font-bold mb-4 text-center sm:text-left">
-//         Dietary Preferences List
-//       </h1>
-//       <div className="overflow-x-auto w-full max-w-md">
-//         <div className="mb-2 flex justify-end">
-//           <div>
-//             <input
-//               type="text"
-//               className="p-2 rounded mr-3 text-gray-800 bg-white text-sm w-42 border-black border-2"
-//               placeholder="New Dietary Preference"
-//               value={newPreference}
-//               onChange={(e) => setNewPreference(e.target.value)}
-//             />
-//           </div>
-//           <div>
-//             <button
-//               onClick={handleCreateDietaryPreferences}
-//               className=" rounded text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black text-center font-bold p-2 w-20"
-//             >
-//               Create
-//             </button>
-//           </div>
-//         </div>
-//         <table className="w-full rounded border-black border-2">
-//           <thead className="bg-cyan-600 font-semibold text-cyan-950 border-black border-2">
-//             <tr>
-//               <th className="px-2 py-1 mb-3 text-center text-xl sm:text-left">
-//                 Dietary Preferences
-//               </th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {mockDietaryPreferences.map((preference) => (
-//               <tr
-//                 key={preference.id}
-//                 className="bg-white border-b border-blue dark:border-blue-600"
-//               >
-//                 <td className="px-3 py-2 text-base text-center sm:text-left">
-//                   {preference.name}
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CreateDietaryPreferences;

21b3375a7025e2e20bccee7f5456bd5f5322a687
Chong Yao Hui <YAO.CHONG891@uowmail.edu.au>
2024-01-14 19:00:51 +0800

    Update system admin view all acc page

--- frontend/app/sysAdmin/userAccount/page.jsx
+++ frontend/app/sysAdmin/userAccount/page.jsx
@@ -1,105 +1,33 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { use } from "react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import axiosInterceptorInstance from "../../axiosInterceptorInstance.js";
 
 // this is the user account list page under sysAdmin
 // router path: /sysAdmin/userAccount
-
-// Mock data for frontend to test the layout
-// need to fetch data from backend (database) in the future
-const mockUserAccountsList = [
-  {
-    id: "3104d2c8-d3d7-41c4-a982-5999a81d7450",
-    username: "admin111",
-    password: "password",
-    email: "mike@gmail.com",
-    isActive: true,
-    userProfile: "System Admin",
-    createdDate: "2019-01-01",
-  },
-  {
-    id: "52be9dca-d141-4eca-9c4d-daed894197c1",
-    username: "user111",
-    password: "password",
-    email: "will@gmail.com",
-    isActive: true,
-    userProfile: "Business User",
-    createdDate: "2022-01-01",
-  },
-  {
-    id: "ede716b9-75da-428d-b163-cc2a62ec1736",
-    username: "business111",
-    password: "password",
-    email: "peter@gmail.com",
-    isActive: true,
-    userProfile: "Dietitian",
-    createdDate: "2022-01-01",
-  },
-  {
-    id: "bd637956-d38b-418d-ad11-60ef002db03b",
-    username: "dietitian111",
-    password: "password",
-    email: "jack@gmail.com",
-    isActive: true,
-    userProfile: "Registered User",
-    createdDate: "2022-01-01",
-  },
-];
 
 const UserAccount = () => {
   const router = useRouter();
   const [userAccounts, setUserAccounts] = useState([]);
 
-  // ready to be tested
-  // // the url will be changed to the backend url
-  // const viewUserAccount = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       "http://localhost:8080/api/userAccount/readAllUserAccounts"
-  //     );
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       // write the data into console for debugging
-  //       console.log("data from backend");
-  //       console.log(data);
-  //       setUserAccounts(data);
-  //     } else {
-  //       console.error("Failed to fetch user profile list");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching data", error);
-  //   }
-  // };
+  // get all the recipes from the backend
+  const viewAllUser = async () => {
+    try {
+      const response = await axiosInterceptorInstance.get(
+        "/systemAdmin/getAllUsers"
+      );
+      console.log(response.data);
+      setUserAccounts(response.data);
+    } catch (error) {
+      console.error("Error fetching data", error);
+    }
+  };
 
-  // // this function is to suspend particular user account
-  // const handleSuspendAccount = async (username) => {
-  //   // Check the username
-  //   console.log(`Username: ${username}`);
-
-  //   // Send a request to backend to suspend the account
-  //   try {
-  //     const response = await fetch(
-  //       `http://localhost:8080/api/userAccount/suspendUserAccount/${username}`,
-  //       {
-  //         method: "PUT",
-  //       }
-  //     );
-  //     if (response.ok) {
-  //       // Refresh the user account list
-  //       viewUserAccount();
-  //     } else {
-  //       console.error("Failed to suspend user account");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error suspending user account", error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   viewUserAccount();
-  // }, []);
+  useEffect(() => {
+    viewAllUser();
+  }, []);
 
   // this function is to view particular user account (the route will be vary based on the user profile type)
   const handleViewAccount = (username, profileType) => {
@@ -159,7 +87,7 @@ const UserAccount = () => {
             </tr>
           </thead>
           <tbody>
-            {mockUserAccountsList.map((user, index) => (
+            {userAccounts.map((user, index) => (
               <tr
                 key={index}
                 className="bg-white border-b border-blue dark:border-blue-600"
@@ -168,7 +96,15 @@ const UserAccount = () => {
                   {user.username}
                 </td>
                 <td className="px-3 py-2 text-base text-center sm:text-left">
-                  {user.userProfile}
+                  {user.role === "ADMIN"
+                    ? "System Admin"
+                    : user.role === "REGISTERED_USER"
+                    ? "Registered User"
+                    : user.role === "BUSINESS_USER"
+                    ? "Business User"
+                    : user.role === "NUTRITIONIST"
+                    ? "Nutrionist"
+                    : "Unknown"}
                 </td>
                 <td className="px-3 py-2 text-base text-center sm:text-left">
                   {user.email}
@@ -179,12 +115,12 @@ const UserAccount = () => {
                 <td className="px-3 py-2 text-base text-center sm:text-left">
                   <span
                     className={`rounded-full px-3 py-1 text-base font-semibold ${
-                      user.isActive === true
+                      user.enabled === true
                         ? "text-white bg-gradient-to-br from-cyan-400 to-cyan-600"
                         : "text-white bg-gradient-to-br from-orange-600 to-red-700"
                     }`}
                   >
-                    {user.isActive === true ? "Active" : "Inactive"}
+                    {user.enabled === true ? "Active" : "Inactive"}
                   </span>
                 </td>
                 <td className="px-3 py-2 flex flex-wrap justify-center sm:justify-start gap-2">
@@ -192,16 +128,16 @@ const UserAccount = () => {
                     onClick={() =>
                       handleViewAccount(user.username, user.userProfile)
                     }
-                    className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-base font-bold px-5 py-2.5 ml-7 mr-7 text-center"
+                    className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-base font-bold px-5 py-2.5 ml-7 mr-7 text-center border-2 border-black"
                   >
                     View
                   </button>
 
                   <button
                     onClick={() => handleSuspendAccount(user.username)}
-                    disabled={!user.isActive} // This will disable the button if user.isActive is false
-                    className={`text-white bg-gradient-to-br from-orange-600 to-red-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-base font-bold px-5 py-2.5 ml-7 mr-7 text-center ${
-                      !user.isActive ? "opacity-50 cursor-not-allowed" : ""
+                    disabled={!user.enabled} // This will disable the button if user.enabled is false
+                    className={`text-white bg-gradient-to-br from-orange-600 to-red-700 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-base font-bold px-5 py-2.5 ml-7 mr-7 text-center border-2 border-black ${
+                      !user.enabled ? "opacity-50 cursor-not-allowed" : ""
                     }`}
                   >
                     Suspend

616d0bcece86514301891402b0592062d493e9b8
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2024-01-15 23:15:50 +0800

    commented out firebaseconfig to remove error lol

--- HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/FirebaseConfig.java
+++ HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/FirebaseConfig.java
@@ -12,21 +12,21 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
-    public FirebaseApp initializeFirebase() throws IOException {
-        InputStream serviceAccount = getClass().getResourceAsStream
-        ("healthy-recipe-fe8f5-firebase-adminsdk-3i4ga-509c39804d.json");
+    // @Bean
+    // public FirebaseApp initializeFirebase() throws IOException {
+    //     InputStream serviceAccount = getClass().getResourceAsStream
+    //     ("healthy-recipe-fe8f5-firebase-adminsdk-3i4ga-509c39804d.json");
 
-        FirebaseOptions options  = FirebaseOptions.builder() 
-                                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                                    // .setProjectId("healthy-recipe-fe8f5")
-                                    .setStorageBucket("healthy-recipe-fe8f5.appspot.com")
-                                    // .setServiceAccountId("my-client-id@my-project-id.iam.gserviceaccount.com")
-                                    .build();
+    //     FirebaseOptions options  = FirebaseOptions.builder() 
+    //                                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+    //                                 // .setProjectId("healthy-recipe-fe8f5")
+    //                                 .setStorageBucket("healthy-recipe-fe8f5.appspot.com")
+    //                                 // .setServiceAccountId("my-client-id@my-project-id.iam.gserviceaccount.com")
+    //                                 .build();
 
-                            // .setCredentials(GoogleCredentials.getApplicationDefault())
-                            // .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
-                            // .build();  
-        return FirebaseApp.initializeApp(options);
-    }
+    //                         // .setCredentials(GoogleCredentials.getApplicationDefault())
+    //                         // .setDatabaseUrl("https://<DATABASE_NAME>.firebaseio.com/")
+    //                         // .build();  
+    //     return FirebaseApp.initializeApp(options);
+    // }
 }

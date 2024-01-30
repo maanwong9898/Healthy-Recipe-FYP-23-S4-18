32a71a865b6a2d930157e0cf20df437758826fea
maanwong <43668754+maanwong@users.noreply.github.com>
2024-01-30 11:43:07 +0800

    Add Drop tables sql

---
+++ sqls/drop_tables.sql
@@ -0,0 +1,32 @@
+drop table user_allergies;
+drop table user_info_over_time;
+drop table allergies;
+drop table blog;
+drop table blog_post_category;
+drop table business_user;
+drop table dietary_preferences;
+drop table educational_content;
+drop table educational_content_category;
+drop table educational_content_review_rating;
+drop table health_goal;
+drop table meal_plan;
+drop table mealplan_recipe;
+drop table meal_plan_review_rating;
+drop table nutritionist;
+drop table recipe;
+drop table recipe_allergies;
+drop table recipe_review_rating;
+drop table registered_user;
+drop table system_admin;
+drop table useraccount;
+
+
+
+
+
+
+
+
+
+
+

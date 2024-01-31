95b0acc38af403b9af5edb13a8b93693f51730bb
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2024-01-31 12:16:19 +0800

    removal of firebase

--- HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/Service/FirebaseStorageService.java
+++ HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/Service/FirebaseStorageService.java
@@ -1,65 +1,65 @@
-package com.FYP18.HealthyRecipe.Service;
+// package com.FYP18.HealthyRecipe.Service;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import com.google.firebase.cloud.StorageClient;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.cloud.storage.Blob;
+// import com.google.cloud.storage.BlobId;
+// import com.google.cloud.storage.BlobInfo;
+// import com.google.cloud.storage.Bucket;
+// import com.google.cloud.storage.Storage;
+// import com.google.cloud.storage.StorageOptions;
+// import com.google.firebase.cloud.StorageClient;
 
-import org.springframework.security.crypto.codec.Utf8;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.security.crypto.codec.Utf8;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.util.UUID;
+// import java.io.File;
+// import java.io.FileInputStream;
+// import java.io.FileOutputStream;
+// import java.io.IOException;
+// import java.nio.charset.StandardCharsets;
+// import java.nio.file.Path;
+// import java.util.UUID;
 
-@Service
-public class FirebaseStorageService {
+// @Service
+// public class FirebaseStorageService {
 
-    private final Storage storage;
+//     private final Storage storage;
 
-    private final String recipeFolder = "Recipes/";    
-    private final String nutritionistFolder = "Recipes/";
-    private final String blogs = "Blogs/";
+//     private final String recipeFolder = "Recipes/";    
+//     private final String nutritionistFolder = "Recipes/";
+//     private final String blogs = "Blogs/";
  
-    // Type -> userid -> 
-    public FirebaseStorageService() {
-        this.storage = StorageOptions.getDefaultInstance().getService();
-    }
-    private final String bucketName = "healthy-recipe-fe8f5.appspot.com";
+//     // Type -> userid -> 
+//     public FirebaseStorageService() {
+//         this.storage = StorageOptions.getDefaultInstance().getService();
+//     }
+//     private final String bucketName = "healthy-recipe-fe8f5.appspot.com";
  
-    public String uploadFile(MultipartFile file) throws IOException {  
+//     public String uploadFile(MultipartFile file) throws IOException {  
  
-        Bucket bucket = StorageClient.getInstance().bucket();
+//         Bucket bucket = StorageClient.getInstance().bucket();
 
-        Blob  blob = bucket.create("Nutritionists/" +file.getOriginalFilename(), file.getBytes(), file.getContentType());
+//         Blob  blob = bucket.create("Nutritionists/" +file.getOriginalFilename(), file.getBytes(), file.getContentType());
 
-        return blob.getMediaLink();
-    }
+//         return blob.getMediaLink();
+//     }
 
-    public String uploadRecipeImages(MultipartFile file, String userId, String recipeId) throws IOException {  
+//     public String uploadRecipeImages(MultipartFile file, String userId, String recipeId) throws IOException {  
  
-        Bucket bucket = StorageClient.getInstance().bucket();
+//         Bucket bucket = StorageClient.getInstance().bucket();
 
-        Blob blob = bucket.create(recipeFolder + userId + recipeId, file.getBytes(), file.getContentType());
+//         Blob blob = bucket.create(recipeFolder + userId + recipeId, file.getBytes(), file.getContentType());
 
-        return blob.getMediaLink();
-    }
+//         return blob.getMediaLink();
+//     }
     
  
-    public void downloadFile(String fileName, Path destinationPath) throws IOException {
+//     public void downloadFile(String fileName, Path destinationPath) throws IOException {
  
-        BlobId blobId = BlobId.of(bucketName, fileName);
-        Blob blob = storage.get(blobId);
+//         BlobId blobId = BlobId.of(bucketName, fileName);
+//         Blob blob = storage.get(blobId);
 
-        blob.downloadTo(destinationPath);
-    }
-}
+//         blob.downloadTo(destinationPath);
+//     }
+// }

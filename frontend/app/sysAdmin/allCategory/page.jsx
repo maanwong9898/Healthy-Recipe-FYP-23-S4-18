4fcadee859de00a48209e79fbee05ab09f44fa62
Chong Yao Hui <YAO.CHONG891@uowmail.edu.au>
2024-02-02 12:01:30 +0800

    Update create meal type and view only for health goal

--- frontend/app/sysAdmin/allCategory/page.jsx
+++ frontend/app/sysAdmin/allCategory/page.jsx
@@ -2,14 +2,30 @@
 import axiosInterceptorInstance from "../../axiosInterceptorInstance.js";
 import { useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 
-const CategoriesPage = () => {
+// router path: /sysAdmin/allCategory
+const AllCategories = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [subcategories, setSubcategories] = useState([]);
   const [newSubcategoryName, setNewSubcategoryName] = useState("");
   const [editingSubcategoryId, setEditingSubcategoryId] = useState(null);
   const [editedSubcategoryName, setEditedSubcategoryName] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    // This effect runs once on component mount
+    const token = localStorage.getItem("token"); // Replace with your token retrieval logic
+    if (!token) {
+      // No token found, redirect to the home page
+      router.push("/"); // Redirect to the home page
+    } else {
+      console.log("Token found:", token);
+      // Token is found, fetch subcategories as usual
+      if (selectedCategory) {
+        fetchSubcategories(selectedCategory);
+      }
+    }
+  }, [selectedCategory, router]);
 
   // Fetch subcategories whenever the selected category changes
   useEffect(() => {
@@ -18,13 +34,14 @@ const CategoriesPage = () => {
     }
   }, [selectedCategory]);
 
-  // Mapping of categories to their respective API endpoints
+  // Mapping of categories to their respective API endpoints (for fetching subcategories)
   const categoryToEndpointMap = {
     DietaryPreferences: "DietaryPreferences",
     HealthGoals: "HealthGoals",
     Allergies: "Allergies",
     BusinessBlogPosts: "BlogPostCategories",
     EducationalContent: "EducationalContentCategories",
+    MealTypes: "MealTypes",
   };
 
   // Function to fetch subcategories based on the selected category
@@ -60,7 +77,7 @@ const CategoriesPage = () => {
     }
   };
 
-  // Function to post a new subcategory to the server
+  // Function to post a new subcategory to the server (called by handleCreateSubcategory)
   const postSubcategory = async (categoryType, subcategoryName) => {
     const endpointMap = {
       DietaryPreferences: "category/dietaryPreference",
@@ -68,6 +85,7 @@ const CategoriesPage = () => {
       Allergies: "category/allergy",
       BusinessBlogPosts: "category/blogPost",
       EducationalContent: "category/educational",
+      MealTypes: "category/mealType",
     };
 
     const endpoint = endpointMap[categoryType];
@@ -101,6 +119,7 @@ const CategoriesPage = () => {
       Allergies: "category/allergy/edit",
       BusinessBlogPosts: "category/blogPostCategory/edit",
       EducationalContent: "category/educationalContentCategory/edit",
+      MealTypes: "category/mealType/edit",
     };
 
     try {
@@ -126,6 +145,7 @@ const CategoriesPage = () => {
       Allergies: `/category/deleteAllergy/${id}`,
       BusinessBlogPosts: `/category/deleteBlogPostCategory/${id}`,
       EducationalContent: `/category/deleteEducationalContent/${id}`,
+      MealTypes: `/category/deleteMealType/${id}`,
     };
 
     try {
@@ -147,6 +167,7 @@ const CategoriesPage = () => {
     "Allergies",
     "BusinessBlogPosts",
     "EducationalContent",
+    "MealTypes",
   ];
 
   // Utility function to format the display names for categories
@@ -157,9 +178,11 @@ const CategoriesPage = () => {
       .trim();
   };
 
+  // Check if the selected category is HealthGoals
+  const isHealthGoals = selectedCategory === "HealthGoals";
   return (
-    <div className="bg-cyan-800 w-full min-h-screen p-4">
-      <h1 className="text-3xl text-white font-bold mb-4">Select a Category</h1>
+    <div className="w-full min-h-screen p-4">
+      <h1 className="text-3xl font-bold mb-4">Select a Category</h1>
       <select
         value={selectedCategory}
         onChange={handleCategoryChange}
@@ -170,98 +193,98 @@ const CategoriesPage = () => {
           <option key={categoryKey} value={categoryKey}>
             {formatDisplayName(categoryKey)}
           </option>
-        ))}
+        ))}{" "}
       </select>
 
-      {selectedCategory && (
-        <>
-          <div className="flex items-center mb-10">
-            <input
-              type="text"
-              value={newSubcategoryName}
-              onChange={(e) => setNewSubcategoryName(e.target.value)}
-              className="p-2 rounded border-2 mr-2"
-              placeholder="New Subcategory Name"
-            />
-            <button
-              onClick={handleCreateSubcategory}
-              className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
-            >
-              Create
-            </button>
-          </div>
-
-          <h2 className="text-2xl font-bold capitalize mb-5 text-white">
-            {formatDisplayName(selectedCategory)} Category
-          </h2>
-          <table className="sm:w-3/4 md:w-2/6 w-full border-black border-2">
-            <thead className="bg-cyan-600 font-semibold text-cyan-950 border-black border-2">
-              <tr>
-                <th className="px-4 py-2 text-xl text-left">Subcategories</th>
-                <th className="px-4 py-2 text-xl text-left"></th>
-                <th className="px-4 py-2 text-xl text-left"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {subcategories.map((subcategory) => (
-                <tr
-                  key={subcategory.id}
-                  className={`border-t bg-white border-b border-blue dark:border-blue-600 ${
-                    editingSubcategoryId === subcategory.id ? "bg-gray-300" : ""
-                  }`}
-                >
-                  <td className="px-4 py-9 text-base">
-                    {editingSubcategoryId === subcategory.id ? (
-                      <input
-                        type="text"
-                        value={editedSubcategoryName}
-                        onChange={(e) =>
-                          setEditedSubcategoryName(e.target.value)
-                        }
-                        className="p-2 rounded border-2"
-                      />
-                    ) : (
-                      subcategory.subcategoryName
-                    )}
-                  </td>
-                  <td>
-                    {editingSubcategoryId === subcategory.id ? (
-                      <button
-                        className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
-                        onClick={() => handleSaveSubcategory(subcategory.id)}
-                      >
-                        Save
-                      </button>
-                    ) : (
-                      <button
-                        className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
-                        onClick={() =>
-                          handleEditSubcategory(
-                            subcategory.id,
-                            subcategory.subcategoryName
-                          )
-                        }
-                      >
-                        Edit
-                      </button>
-                    )}
-                  </td>
-                  <td>
-                    <button
-                      className="text-white bg-red-600 border-2 border-black font-bold py-2 px-4 rounded"
-                      onClick={() => handleDeleteSubcategory(subcategory.id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
+      {selectedCategory && !isHealthGoals && (
+        <div className="flex items-center mb-10">
+          <input
+            type="text"
+            value={newSubcategoryName}
+            onChange={(e) => setNewSubcategoryName(e.target.value)}
+            className="p-2 rounded border-2 mr-2"
+            placeholder="New Subcategory Name"
+          />
+          <button
+            onClick={handleCreateSubcategory}
+            className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
+          >
+            Create
+          </button>
+        </div>
       )}
+
+      <h2 className="text-2xl font-bold capitalize mb-5 ">
+        {formatDisplayName(selectedCategory)} Category
+      </h2>
+      <table className="sm:w-3/4 md:w-2/6 w-full border-black border-2">
+        <thead className="bg-zinc-700 font-normal text-white border-gray-800 border-2">
+          <tr>
+            <th className="px-4 py-2 text-xl text-left">Subcategories</th>
+            <th className="px-4 py-2 text-xl text-left"></th>
+            <th className="px-4 py-2 text-xl text-left"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {subcategories.map((subcategory) => (
+            <tr
+              key={subcategory.id}
+              className={`border-t bg-white border-b border-blue dark:border-blue-600 ${
+                editingSubcategoryId === subcategory.id ? "bg-gray-300" : ""
+              }`}
+            >
+              <td className="px-4 py-9 text-base">
+                {editingSubcategoryId === subcategory.id ? (
+                  <input
+                    type="text"
+                    value={editedSubcategoryName}
+                    onChange={(e) => setEditedSubcategoryName(e.target.value)}
+                    className="p-2 rounded border-2"
+                  />
+                ) : (
+                  subcategory.subcategoryName
+                )}
+              </td>
+              <td>
+                {!isHealthGoals && editingSubcategoryId === subcategory.id && (
+                  <button
+                    onClick={() => handleSaveSubcategory(subcategory.id)}
+                    className="text-white
+                     bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
+                  >
+                    Save
+                  </button>
+                )}
+                {!isHealthGoals && editingSubcategoryId !== subcategory.id && (
+                  <button
+                    onClick={() =>
+                      handleEditSubcategory(
+                        subcategory.id,
+                        subcategory.subcategoryName
+                      )
+                    }
+                    className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black hover:bg-blue-700 font-bold py-2 px-4 rounded"
+                  >
+                    Edit
+                  </button>
+                )}
+              </td>
+              <td>
+                {!isHealthGoals && (
+                  <button
+                    onClick={() => handleDeleteSubcategory(subcategory.id)}
+                    className="text-white bg-red-600 border-2 border-black font-bold py-2 px-4 rounded"
+                  >
+                    Delete
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default CategoriesPage;
+export default AllCategories;

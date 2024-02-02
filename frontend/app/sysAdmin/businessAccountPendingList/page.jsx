5fb3121c40008a3df8d9c1d025cf58ef8c5fe690
maanwong <43668754+maanwong@users.noreply.github.com>
2024-02-02 15:34:28 +0800

    Update system Admin verify account pages

--- frontend/app/sysAdmin/businessAccountPendingList/page.jsx
+++ frontend/app/sysAdmin/businessAccountPendingList/page.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import axiosInterceptorInstance from "../../axiosInterceptorInstance.js";
+import SwapVertIcon from "@mui/icons-material/SwapVert";
 
 // router path: /sysAdmin/businessAccountPendingList
 // this is to view the list of business users and dietitians that status are waiting for verification
@@ -14,7 +15,8 @@ import axiosInterceptorInstance from "../../axiosInterceptorInstance.js";
 const sortOptions = {
   LATEST: { key: "LATEST", label: "By Latest" },
   OLDEST: { key: "OLDEST", label: "By Oldest" },
-
+  ALPHABETICAL_AZ: { key: "ALPHABETICAL_AZ", label: "Alphabetically (A to Z)" },
+  ALPHABETICAL_ZA: { key: "ALPHABETICAL_ZA", label: "Alphabetically (Z to A)" },
   // ... add more sorting options if needed ...
 };
 
@@ -24,6 +26,8 @@ const BusinessAccountPendingList = () => {
   const [selectedRole, setSelectedRole] = useState("All");
   const [sortOrder, setSortOrder] = useState("Ascending");
   const [sortOption, setSortOption] = useState(sortOptions.LATEST.key);
+  const [alphabeticalOrder, setAlphabeticalOrder] = useState("AZ");
+  const [createdDateOrder, setCreatedDateOrder] = useState("LATEST");
 
   useEffect(() => {
     const fetchUnverifiedBusinessAccounts = async () => {
@@ -100,35 +104,39 @@ const BusinessAccountPendingList = () => {
     }
   };
 
+  // Sort by alphabetical order
+  const handleSortAlphabetically = () => {
+    if (alphabeticalOrder === "AZ") {
+      setSortOption("ALPHABETICAL_AZ");
+      setAlphabeticalOrder("ZA");
+    } else {
+      setSortOption("ALPHABETICAL_ZA");
+      setAlphabeticalOrder("AZ");
+    }
+  };
+
+  // Sort by created date order
+  const handleSortCreatedDate = () => {
+    if (createdDateOrder === "LATEST") {
+      setSortOption("OLDEST");
+      setCreatedDateOrder("OLDEST");
+    } else {
+      setSortOption("LATEST");
+      setCreatedDateOrder("LATEST");
+    }
+  };
+  
   return (
-    <div className="px-2 sm:px-5 bg-cyan-800 min-h-screen flex flex-col py-5 ">
-      <h1 className="text-2xl text-white font-bold p-3 text-center mb-3 sm:text-left">
+    <div className="px-2 sm:px-5 min-h-screen flex flex-col py-5">
+      <h1 className="text-2xl lg:text-5xl text-gray-900 p-3 mb-4 font-bold text-center sm:text-center">
         Pending Business Account List for Verification
       </h1>
-      <div className="flex justify-end mb-3">
+      <div className="flex flex-col mb-4 md:flex-row md:mr-2">
         {/* Filter dropdown */}
-        {/* <select
-          onChange={(e) => handleRoleFilterChange(e.target.value)}
-          className="mr-2"
-        >
-          <option value="All">All Roles</option>
-          <option value="BUSINESS_USER">Business User</option>
-          <option value="NUTRITIONIST">Nutritionist</option>
-        </select> */}
-
-        {/* Sort dropdown */}
-        {/* <select onChange={(e) => setSortOption(e.target.value)}>
-          {Object.values(sortOptions).map((option) => (
-            <option key={option.key} value={option.key}>
-              {option.label}
-            </option>
-          ))}
-        </select> */}
-        {/* Filter dropdown */}
-        <div className="mb-2 md:mb-0 md:mr-6">
+        <div className="relative md:ml-auto">
           <label
             htmlFor="categoryFilter"
-            className="ml-2 mr-2 font-2xl text-white"
+            className="ml-2 mr-2 font-2xl text-gray-900"
           >
             Filter By:
           </label>
@@ -142,7 +150,8 @@ const BusinessAccountPendingList = () => {
             <option value="NUTRITIONIST">Nutritionist</option>
           </select>
         </div>
-        <div className="mb-2 md:mb-0 md:mr-6">
+        {/* Sort */}
+        {/* <div className="mb-2 md:mb-0 md:mr-6">
           <label htmlFor="sort" className="mr-2 font-2xl text-white">
             Sort By:
           </label>
@@ -158,29 +167,44 @@ const BusinessAccountPendingList = () => {
               </option>
             ))}
           </select>
-        </div>
+        </div> */}
       </div>
-      <div className="overflow-x-auto">
-        <table className="min-w-full rounded-lg border-black border-2">
-          <thead className="bg-cyan-600 font-semibold text-cyan-950 text-lg border-black border-2">
-            <tr>
-              <th className="px-3 py-2 text-left">Username</th>
-              <th className="px-3 py-2 text-left">Profile Type</th>
-              <th className="px-3 py-2 text-left">Email</th>
-              <th className="px-3 py-2 text-left">Created Date</th>
-              <th className="px-3 py-2 text-left"></th>
+
+      {/* Table of accounts */}
+      <div className="overflow-x-auto rounded-lg hidden lg:block">
+        <table className="min-w-full rounded-lg border-zinc-200 border-2">
+          <thead className="bg-zinc-700 font-normal text-white border-gray-800 border-2">
+            <tr className="text-center text-lg">
+              <th className="px-3 py-2">
+                Username
+                <button
+                  className="ml-1 focus:outline-none"
+                  onClick={handleSortAlphabetically}
+                >
+                  <SwapVertIcon />
+                </button>
+              </th>
+              <th className="px-3 py-2">Profile Type</th>
+              <th className="px-3 py-2">Email</th>
+              <th className="px-3 py-2">
+                Created Date
+                <button
+                  className="ml-1 focus:outline-none"
+                  onClick={handleSortCreatedDate}
+                >
+                  <SwapVertIcon />
+                </button>
+              </th>
+              <th className="px-3 py-2"></th>
             </tr>
           </thead>
           <tbody>
             {filteredAndSortedAccounts.map((user, index) => (
-              <tr
-                key={index}
-                className="bg-white border-b border-blue dark:border-blue-600"
-              >
-                <td className="px-3 py-2 text-base text-center sm:text-left">
+              <tr key={index} className="bg-white border-b">
+                <td className="px-3 py-2 text-base text-center">
                   {user.username}
                 </td>
-                <td className="px-3 py-2 text-base text-center sm:text-left">
+                <td className="px-3 py-2 text-base text-center">
                   {user.role === "ADMIN"
                     ? "System Admin"
                     : user.role === "REGISTERED_USER"
@@ -191,22 +215,22 @@ const BusinessAccountPendingList = () => {
                     ? "Nutrionist"
                     : "Unknown"}
                 </td>
-                <td className="px-3 py-2 text-base text-center sm:text-left">
+                <td className="px-3 py-2 text-base text-center">
                   {user.email}
                 </td>
                 {/* <td className="px-3 py-2 text-sm text-center sm:text-left">
                   {user.UEN}
                 </td> */}
-                <td className="px-3 py-2 text-base text-center sm:text-left">
+                <td className="px-3 py-2 text-base text-center">
                   {user.createdDate}
                 </td>
 
-                <td className="px-3 py-2 flex flex-wrap justify-center sm:justify-start gap-2">
+                <td className="px-3 py-2 justify-center sm:justify-start">
                   <button
                     onClick={() =>
                       handleViewUnverifiedAccount(user.id, user.role)
                     }
-                    className="text-white bg-gradient-to-br from-cyan-400 to-cyan-800 border-2 border-black font-bold hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-base px-5 py-2.5 ml-7 mr-7 text-center"
+                    className="text-white font-bold bg-blue-600 hover:bg-blue-700 rounded-lg text-base px-5 py-2 ml-2 mr-2 text-center"
                   >
                     {" "}
                     Verify Details
@@ -216,6 +240,72 @@ const BusinessAccountPendingList = () => {
             ))}
           </tbody>
         </table>
+      </div>
+      {/* Mobile View for Tables */}
+      <div className="mx-auto items-center lg:hidden">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+          {filteredAndSortedAccounts.map((user, index) => (
+            <div
+              key={index}
+              className="bg-white p-5 h-full flex flex-col border border-gray-300 rounded-2xl shadow"
+            >
+              {/* Username */}
+              <p className="px-3 py-2 text-lg">
+                <span className="font-semibold text-gray-900">Username: </span>
+                <span className="font-normal text-gray-900">
+                  {user.username}
+                </span>
+              </p>
+
+              {/* Profile Type */}
+              <p className="px-3 py-2 text-lg">
+                <span className="font-semibold text-gray-900">
+                  Profile Type:{" "}
+                </span>
+                <span className="font-normal text-gray-900">
+                  {user.role === "ADMIN"
+                    ? "System Admin"
+                    : user.role === "REGISTERED_USER"
+                    ? "Registered User"
+                    : user.role === "BUSINESS_USER"
+                    ? "Business User"
+                    : user.role === "NUTRITIONIST"
+                    ? "Nutrionist"
+                    : "Unknown"}
+                </span>
+              </p>
+
+              {/* Email */}
+              <p className="px-3 py-2 text-lg">
+                <span className="font-semibold text-gray-900">Email: </span>
+                <span className="font-normal text-gray-900">{user.email}</span>
+              </p>
+
+              {/* Created Date */}
+              <p className="px-3 py-2 text-lg">
+                <span className="font-semibold text-gray-900">
+                  Created Date:{" "}
+                </span>
+                <span className="font-normal text-gray-900">
+                  {" "}
+                  {user.createdDate}
+                </span>
+              </p>
+
+              {/* Button */}
+              <div className="mt-2 flex flex-col space-y-3 items-center">
+                <button
+                  onClick={() =>
+                    handleViewUnverifiedAccount(user.id, user.role)
+                  }
+                  className="text-white font-bold bg-blue-600 hover:bg-blue-700 rounded-lg text-base w-full px-5 py-2 ml-2 mr-2 text-center"
+                >
+                  Verify Details
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );

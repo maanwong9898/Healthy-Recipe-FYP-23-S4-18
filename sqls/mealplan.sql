6e07df301a4c2359623c17c5e487bc1698a63d46
minnnn99 <131181468+minnnn99@users.noreply.github.com>
2024-02-03 16:05:56 +0800

    Update mealplan.sql
    add mealplan data

--- sqls/mealplan.sql
+++ sqls/mealplan.sql
@@ -29,6 +29,20 @@ VALUES
         'https://img.freepik.com/free-vector/main-food-groups-macronutrients-vector_1308-127634.jpg?size=626&ext=jpg&ga=GA1.1.1497591401.1706588994&semt=ais',
         'Designed by Freepik',  -- img_title
         '5' -- userid
+    ),
+    (
+        2,
+        'Sheeba',
+        'Muscle Gain Meal Plan',
+        TRUE,
+        'Gaining muscle may improve your abilities in a certain sport or enhance your quality of life. Muscle helps protect joints from injury, improves balance, and reduces the risk of falls. Muscle mass also plays an important role in physical and metabolic health.',
+        'Adequate calories are required to grow muscle, and you may need to increase your total calorie consumption to achieve your goal. If you are on a diet to gain muscle, you might want to think about consuming 400 to 600 calories every meal and 150 to 400 calories for snacks. At every meal and snack, eating the appropriate proportions of fat, protein, and carbohydrates will help you feel fuller for longer.',
+        'By prioritizing a well-rounded consumption of macronutrients, encompassing sufficient protein, carbohydrates, and beneficial fats, in addition to incorporating micronutrient-dense foods, individuals can supply their bodies with the essential fuel required for optimal muscle growth and recovery.',
+        '2023-06-15',
+        1, -- weight loss
+        'https://img.freepik.com/free-photo/flat-lay-fish-vegetables_23-2148262195.jpg?w=1060&t=st=1706947179~exp=1706947779~hmac=1d8ec88d4d3ce2cb4c97c6043dd1c10eaab11d5d6dec821c90cb1f49346c60be',
+        'Designed by Freepik',  -- img_title
+        '5' -- userid
     );
     
 
@@ -42,8 +56,8 @@ values
     (1, 10),
     (1, 18),
     (2, 4),
-    (2, 5),
-    (2, 6),
+    (2, 17),
+    (2, 20),
     (3, 7),
     (3, 8),
     (3, 9),

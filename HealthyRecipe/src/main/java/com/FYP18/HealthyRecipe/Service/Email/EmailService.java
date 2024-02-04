c495ba973adcb2e17d7d441993a8d770ff8fa32d
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2024-02-04 22:24:53 +0800

    verified // reject email for nutritionist + business user

--- HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/Service/Email/EmailService.java
+++ HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/Service/Email/EmailService.java
@@ -14,7 +14,52 @@ public class EmailService {
     private SendGrid sendGrid;
   
     private final Email from = new Email("myhealthyrecipes23418@gmail.com");
- 
+    
+    // only business / nutritionist will call these 2
+    public void sendVerifiedEmail(String to) throws Exception{
+
+        Email toEmail = new Email(to);
+        String subject = "Healthy Recipe - registration accepted";
+        String body ="Thank you for applying to Healthy Recipe! \nYour registration to Healthy Recipe is successful. You may now login to Healthy Recipe.\n"
+                    +"\n\nThank you,\nHealthy Recipe Team";
+
+        Content content = new Content("text/plain", body);
+        Mail mail = new Mail(from, subject, toEmail, content);
+        
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        Response response = sendGrid.api(request);
+        if (response.getStatusCode() >= 200 && response.getStatusCode() < 300) {
+            System.out.println("Email sent successfully");
+        } else {
+            System.out.println("Error sending email. Status code: " + response.getStatusCode());
+        }
+    }
+    public void sendRejectionEmail(String to) throws Exception{
+
+        Email toEmail = new Email(to);
+        String subject = "Healthy Recipe's registration rejected";
+        String body ="Thank you for applying to Healthy Recipe! \nYour registration to Healthy Recipe is unsuccessful and is deleted.\n"
+                    +"\n\nThank you,\nHealthy Recipe Team";
+
+        Content content = new Content("text/plain", body);
+        Mail mail = new Mail(from, subject, toEmail, content);
+
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        Response response = sendGrid.api(request);
+        if (response.getStatusCode() >= 200 && response.getStatusCode() < 300) {
+            System.out.println("Email sent successfully");
+        } else {
+            System.out.println("Error sending email. Status code: " + response.getStatusCode());
+        }
+    }
     public void sendVerificationEmail(String to, String link) throws Exception {
         Email toEmail = new Email(to);
         String subject = "Sign up for Healthy Recipe - Email Verification";

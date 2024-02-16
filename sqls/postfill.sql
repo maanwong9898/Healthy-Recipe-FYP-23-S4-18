1fb7eafc5c7593a2b3883ce22710abe169027d96
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2024-02-16 15:54:39 +0800

    after calling fill.sql, should call this sql

---
+++ sqls/postfill.sql
@@ -0,0 +1,25 @@
+UPDATE ALLERGIES SET subcategory_name = "TWlsaw==" WHERE id = 1;
+UPDATE ALLERGIES SET subcategory_name = "RWdn" WHERE id = 2;
+UPDATE ALLERGIES SET subcategory_name = "U295" WHERE id = 3;
+UPDATE ALLERGIES SET subcategory_name = "U2hlbGxmaXNo" WHERE id = 4;
+UPDATE ALLERGIES SET subcategory_name = "RmlzaA==" WHERE id = 5;
+UPDATE ALLERGIES SET subcategory_name = "UGVhbnV0" WHERE id = 6;
+UPDATE ALLERGIES SET subcategory_name = "VHJlZSBOdXRz" WHERE id = 7;
+UPDATE ALLERGIES SET subcategory_name = "R2x1dGVu" WHERE id = 8;
+UPDATE ALLERGIES SET subcategory_name = "V2hlYXQ=" WHERE id = 9;
+UPDATE ALLERGIES SET subcategory_name = "RGFpcnk=" WHERE id = 10;
+UPDATE ALLERGIES SET subcategory_name = "TGVndW1l" WHERE id = 11;
+ 
+UPDATE USERACCOUNT SET PASSWORD = "$2a$10$O2YyQF9kbsbcOxW.0pnmWey3b7Iz7eaq6rtb1YE3t9K0bXaVyuqBu";
+
+-- UPDATE ALLERGIES SET subcategory_name = "Milk" WHERE id = 1;
+-- UPDATE ALLERGIES SET subcategory_name = "Egg" WHERE id = 2;
+-- UPDATE ALLERGIES SET subcategory_name = "Soy" WHERE id = 3;
+-- UPDATE ALLERGIES SET subcategory_name = "Shellfish" WHERE id = 4;
+-- UPDATE ALLERGIES SET subcategory_name = "Fish" WHERE id = 5;
+-- UPDATE ALLERGIES SET subcategory_name = "Peanut" WHERE id = 6;
+-- UPDATE ALLERGIES SET subcategory_name = "Tree Nuts" WHERE id = 7;
+-- UPDATE ALLERGIES SET subcategory_name = "Gluten" WHERE id = 8;
+-- UPDATE ALLERGIES SET subcategory_name = "Wheat" WHERE id = 9;
+-- UPDATE ALLERGIES SET subcategory_name = "Dairy" WHERE id = 10;
+-- UPDATE ALLERGIES SET subcategory_name = "Legume" WHERE id = 11;

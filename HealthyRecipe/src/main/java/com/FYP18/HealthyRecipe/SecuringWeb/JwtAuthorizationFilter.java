4fb0a1727caf02576ee415866c561e32fe4765cb
Jia Zheng <33411360+jahhzhengc@users.noreply.github.com>
2024-02-17 15:36:38 +0800

    clean up securities file

--- HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/JwtAuthorizationFilter.java
+++ HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/JwtAuthorizationFilter.java
@@ -2,36 +2,35 @@ package com.FYP18.HealthyRecipe.SecuringWeb;
   
 import com.FYP18.HealthyRecipe.Service.CustomUserDetailService; 
 import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.google.firebase.auth.FirebaseAuth;
+// import com.google.firebase.auth.FirebaseAuthException;
+// import com.google.firebase.auth.FirebaseToken;
+
+
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse; 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.http.HttpServletResponse;  
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.http.MediaType; 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.core.userdetails.UserDetails; 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.io.IOException; 
+import java.util.HashMap; 
 import java.util.Map; 
-
+ 
 @Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final ObjectMapper mapper;
+    private final CustomUserDetailService userDetailService;
 
     public JwtAuthorizationFilter(JwtUtil jwtUtil, 
                                     ObjectMapper mapper,
@@ -41,56 +40,47 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         this.mapper = mapper;
         this.userDetailService = userDetailService;
     }
-    
-    // @Autowired
-    private final 
-    // public 
-    CustomUserDetailService userDetailService;
+     
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, 
                                     HttpServletResponse response, 
-                                    FilterChain filterChain) throws ServletException, IOException {
-         
-                                        
+                                    FilterChain filterChain) throws ServletException, IOException
+                                     {
+          
         Map<String, Object> errorDetails = new HashMap<>();
-        // Add debug statements
-         
-        // try { 
+         try { 
             String accessToken = jwtUtil.resolveToken(request);  
+
             if (accessToken == null ) {
                 filterChain.doFilter(request, response);
                 return;
-            } 
+            }   
+
             Claims claims = jwtUtil.resolveClaims(request);
             Boolean validate= jwtUtil.validateClaims(claims);
-            // System.out.println("AMMMM" + claims + " " + validate);
-            System.out.println("AUTH:" +SecurityContextHolder.getContext().getAuthentication());
-            System.out.println(claims.getExpiration() + " EXPIRED: " +claims.getExpiration().before(new Date()));
+ 
             if(claims != null & validate){ 
                 String email = claims.getSubject();
-                // System.out.println("EMAIL: " +email);
-                UserDetails user = userDetailService.loadUserByUsername(email);
+                 UserDetails user = userDetailService.loadUserByUsername(email);
                  
                 Authentication authentication =
                         new UsernamePasswordAuthenticationToken(email,null,
                         user.getAuthorities()); 
-                System.out.println("getAuthorities: " + authentication.getAuthorities());
-                
+                 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                System.out.println("AUTH2:" +SecurityContextHolder.getContext().getAuthentication());
-
+ 
             }
 
-        // }catch (Exception e){
-        //     errorDetails.put("message", "LAuthentication Error");
-        //     errorDetails.put("details",e.getMessage());
-        //     response.setStatus(HttpStatus.FORBIDDEN.value());
-        //     response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        }catch (Exception e){ 
+            errorDetails.put("details",e.getMessage()); 
+            e.printStackTrace();
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
-        //     mapper.writeValue(response.getWriter(), errorDetails);
+            mapper.writeValue(response.getWriter(), errorDetails);
 
-        // }
+        }
         filterChain.doFilter(request, response);
     }
 }
--- HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/WebSecurityConfig.java
+++ HealthyRecipe/src/main/java/com/FYP18/HealthyRecipe/SecuringWeb/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.FYP18.HealthyRecipe.SecuringWeb;
-
-import java.beans.Customizer;
+ 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,168 +8,44 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity; 
+import org.springframework.security.config.http.SessionCreationPolicy; 
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
+import org.springframework.security.crypto.password.PasswordEncoder; 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.FYP18.HealthyRecipe.Repository.UserRepository;
-import com.FYP18.HealthyRecipe.Service.CustomUserDetailService;
+ 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-    // passwordEncoder -> securityFilterChain -> configure -> filterChain ->
-    // authenticationManager
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-    // Exception
-    // {
-    // System.out.println("securityFilterChain");
-    // http
-    // .authorizeHttpRequests((authorize) -> authorize
-
-    // // .requestMatchers("/login").permitAll()
-    // .anyRequest().authenticated()
-    // );
-
-    // return http.build();
-    // }
-    // private final CustomUserDetailService userDetailsService;
-    // private final JwtAuthorizationFilter jwtAuthorizationFilter;
-
-    // public WebSecurityConfig(CustomUserDetailService customUserDetailsService)
-    // //, JwtAuthorizationFilter jwtAuthorizationFilter)
-    // {
-    // this.userDetailsService = customUserDetailsService;
-    // // this.jwtAuthorizationFilter = jwtAuthorizationFilter;
-    // }
-
-    // @Autowired
-    // public UserRepository userRepository;
-
-    // @Bean
-    // public CustomUserDetailService userDetailService()
-    // {
-    // return new CustomUserDetailService(userRepository);
-    // }
-
-    @Bean
-    // @SuppressWarnings("deprecation")
+  
+    @Bean 
     public PasswordEncoder passwordEncoder() {
-
-        System.out.println("passwordEncoder");
-        // return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-        return new BCryptPasswordEncoder(); //
-        // PasswordEncoderFactories.createDelegatingPasswordEncoder();
+ 
+        return new BCryptPasswordEncoder(); 
     }
 
     @Bean
     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
             PasswordEncoder passwordEncoder) {
-
-        System.out.println("authenticationManager");
-
+  
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return new ProviderManager(authenticationProvider);
     }
-
-    // @Bean
-    // public SecurityFilterChain configure (HttpSecurity http) throws Exception
-    // {
-    // System.out.println("configure");
-    // http
-    // .csrf(csrf-> csrf.disable())
-    // .sessionManagement(x->
-    // x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    // .authorizeHttpRequests((authz)->
-    // authz
-    // .anyRequest()
-    // // .requestMatchers("/public")
-    // .permitAll()
-    // // .anyRequest()
-    // // .authenticated()
-    // );
-
-    // return http.build();
-    // }
-    // @Bean
-    // public UserDetailsService users()
-    // {
-    // UserDetails admin = User.builder()
-    // .username("admin")
-    // .password("pw")
-    // .roles("ADMIN")
-    // .build();
-    // UserDetails user = User.builder()
-    // .username("user")
-    // .password("pw")
-    // .roles("USER")
-    // .build();
-    // return new InMemoryUserDetailsManager(admin, user);
-    // }
-
-    // @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-    // System.out.println("filterChain");
-
-    // http.authorizeHttpRequests(req->
-    // req.anyRequest().authenticated());
-
-    // // http
-    // // .csrf()
-    // // .formLogin()
-    // // .and()
-    // // .headers().disable()
-    // // .csrf().disable()
-    // // .antMatcher("/**")
-    // // .authorizeRequests()
-    // // .antMatchers("/public").permitAll()
-    // // .antMatchers("/h2-console/**").permitAll()
-    // // //...
-    // // .anyRequest()
-    // // .authenticated();
-    // return http.build();
-    // }
+ 
     @Autowired
     public JwtAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        System.out.println("FilterChain");
-        http
-                .csrf(csrf -> csrf.disable())
-                // Disabling CSRF protection means that Spring Security won't generate and
-                // validate these tokens.
-                // While this solves the issue for now, it's important to understand that
-                // disabling CSRF protection
-                // can expose your application to CSRF attacks. It's generally recommended to
-                // keep CSRF protection
-                // enabled and properly configure your frontend code to include and send the
-                // CSRF token with each request.
+         http
+                .csrf(csrf -> csrf.disable()) 
 
-                .authorizeHttpRequests((authz) -> authz
-                        // .anyRequest().permitAll()
-                        // .requestMatchers("/login").permitAll()
-                        // .requestMatchers("/test/admin").hasRole("ADMIN")
-                        // .requestMatchers("/test/business").hasRole("BUSINESS_USER")
-                        // .requestMatchers("/test/nut").hasRole("NUTRITIONIST")
-                        // .requestMatchers("/test/user").hasRole("REGISTERED_USER")
-                        // .requestMatchers("/test/userAndAdmin").hasAnyRole("ADMIN", "USER")
-                        // .requestMatchers("/register/**").permitAll()
+                .authorizeHttpRequests((authz) -> authz 
                         .anyRequest().permitAll())
 
                 .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
